bc36d91e2e03be8bd944015d9b587faf37bc3bd9
Gabriel <g.millaquiensuarez@gmail.com>
2019-05-16 23:31:02 -0400

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,56 @@
+<!--
+    Author: Gabriel M
+    Author URL:  
+    License: Creative Commons Attribution 3.0 Unported
+    License URL: http://creativecommons.org/licenses/by/3.0/
+    -->
+    <!DOCTYPE>
+    <html lang="es">
+    
+    <head>
+        <title>Adturn</title>
+        <!-- Meta Tags -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="keywords" content=" " />
+        <script>
+            addEventListener("load", function () {
+                setTimeout(hideURLbar, 0);
+            }, false);
+    
+            function hideURLbar() {
+                window.scrollTo(0, 1);
+            }
+        </script>
+        <!-- //Meta Tags -->
+    
+        <!-- Style-sheets -->
+        <!-- Bootstrap Css -->
+        <link href=" {{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
+        <!-- Bootstrap Css -->
+        <!-- Bars Css -->
+        <link rel="stylesheet" href="{{ asset('css/bar.css') }}">
+        <!--// Bars Css -->
+        <!-- Calender Css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/pignose.calender.css') }}" />
+        <!--// Calender Css -->
+        <!-- Common Css -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+        <!--// Common Css -->
+        <!-- Nav Css -->
+        <link rel="stylesheet" href="{{ asset('css/style4.css') }}">
+        <!--// Nav Css -->
+        <!-- Fontawesome Css -->
+        <link href="{{ asset('css/fontawesome-all.css') }}" rel="stylesheet">
+        <!--// Fontawesome Css -->
+        <!--// Style-sheets -->
+    
+        <!--web-fonts-->
+        <link href="{{ asset('//fonts.googleapis.com/css?family=Poiret+One') }}" rel="stylesheet">
+        <link href="{{ asset('//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css') }}">
+         
+        <script src="{{ asset('https://code.jquery.com/jquery-1.12.4.js') }}"></script>
+        <script src="{{ asset('https://code.jquery.com/ui/1.12.1/jquery-ui.js') }}"></script>
+        <!--//web-fonts-->
+    </head>
